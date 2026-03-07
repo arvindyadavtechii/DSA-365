b2d68d9b86c37340f78a8cfa00a3a41ce66af0bd
Arvind Yadav <arvindyadavtechii@gmail.com>
2026-03-07 22:00:54 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (7.88%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.js
@@ -0,0 +1,20 @@
+var isSymmetric = function(root) {
+
+    function isMirror(left, right){
+        if(left === null && right === null){
+            return true;
+        }
+
+        if(left === null || right === null){
+            return false;
+        }
+
+        return (
+            left.val === right.val &&
+            isMirror(left.left, right.right) &&
+            isMirror(left.right, right.left)
+        );
+    }
+
+    return isMirror(root.left, root.right);
+};
