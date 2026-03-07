6c4ac06f426d190a51ae67b460564f1f38a10c5a
Arvind Yadav <arvindyadavtechii@gmail.com>
2026-03-07 22:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 59.6 MB (25.75%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -0,0 +1,22 @@
+var levelOrder = function(root) {
+
+    let result = [];
+
+    function dfs(node, level){
+
+        if(!node) return;
+
+        if(result.length === level){
+            result.push([]);
+        }
+
+        result[level].push(node.val);
+
+        dfs(node.left, level + 1);
+        dfs(node.right, level + 1);
+    }
+
+    dfs(root, 0);
+
+    return result;
+};
