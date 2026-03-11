2a457b68fe95fbb767be1116286e1f8e9541a2ab
Arvind Yadav <arvindyadavtechii@gmail.com>
2026-03-11 20:24:30 +0530

    Time: 1 ms (25.22%) | Memory: 55.5 MB (98.93%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,16 @@
+var flatten = function(root) {
+    let prev = null;
+
+    function dfs(node) {
+        if (!node) return;
+
+        dfs(node.right);
+        dfs(node.left);
+
+        node.right = prev;
+        node.left = null;
+        prev = node;
+    }
+
+    dfs(root);
+};
