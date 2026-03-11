40bfc26f89fa68656a2a52faaf9c138dfcaf4115
Arvind Yadav <arvindyadavtechii@gmail.com>
2026-03-11 20:22:46 +0530

    Time: 1 ms (58.36%) | Memory: 58.8 MB (31.25%) - LeetSync

---
+++ 112-path-sum/path-sum.js
@@ -0,0 +1,13 @@
+var hasPathSum = function(root, targetSum) {
+    if (root === null) {
+        return false;
+    }
+
+    // Check leaf node
+    if (root.left === null && root.right === null) {
+        return targetSum === root.val;
+    }
+
+    return hasPathSum(root.left, targetSum - root.val) ||
+           hasPathSum(root.right, targetSum - root.val);
+};
