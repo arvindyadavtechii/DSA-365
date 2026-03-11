a844a9fbf0c8c3d9cbf7244fcc772d1114ce6b99
Arvind Yadav <arvindyadavtechii@gmail.com>
2026-03-11 20:25:40 +0530

    Time: 63 ms (26.96%) | Memory: 60.2 MB (74.61%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,22 @@
+var connect = function(root) {
+    if (!root) return null;
+
+    let queue = [root];
+
+    while (queue.length) {
+        let size = queue.length;
+
+        for (let i = 0; i < size; i++) {
+            let node = queue.shift();
+
+            if (i < size - 1) {
+                node.next = queue[0];
+            }
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+
+    return root;
+};
