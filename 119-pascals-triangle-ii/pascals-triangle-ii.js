2c5772543e5a0fda6bc5358dad0aa1b14dead879
Arvind Yadav <arvindyadavtechii@gmail.com>
2026-03-11 20:26:57 +0530

    Time: 0 ms (100.00%) | Memory: 54.7 MB (14.69%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -0,0 +1,11 @@
+var getRow = function(rowIndex) {
+    let row = new Array(rowIndex + 1).fill(1);
+
+    for (let i = 2; i <= rowIndex; i++) {
+        for (let j = i - 1; j > 0; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+    }
+
+    return row;
+};
